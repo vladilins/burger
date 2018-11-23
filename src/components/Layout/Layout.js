f0395d028ce4bde581adca66313bb63783df0df3
Vlad Ilin <ilin96inbox@gmail.com>
2018-11-23 19:30:20 +0800

    new burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Auxa from "../../hoc/Auxa";
+
 import classes from "./Layout.css";
 
 const layout = props => (
-  <Auxa>
-    <div>Toolbar, Sidbar, Backdrop</div>
+  <>
+    <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
-  </Auxa>
+  </>
 );
 
 export default layout;
--- src/containers/BorgerBuilder/BurgerBuilder.js
+++ src/containers/BorgerBuilder/BurgerBuilder.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {};
+
   render() {
     return (
       <>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </>
     );
