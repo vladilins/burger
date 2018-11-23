a3ce340c78bce7c058273d9d6c7386f2e30c078a
Vlad Ilin <ilin96inbox@gmail.com>
2018-11-23 18:57:41 +0800

    ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,6 +7,18 @@ const burgerIngredient = props => {
   switch (props.type) {
     case "bread-bottom":
       ingredient = <div className={classes.BreadBottom} />;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1} />
+          <div className={classes.Seeds2} />
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={classesMeat} />;
+      break;
   }
 };
 
